77d97714f405927879f8b448a10625c89a0a3dd0
Bedrockbreaker <coolmandude101@gmail.com>
2026-08-20 15:27:20 -0700

    Fix only looking for command files ending in .ts

--- src/getCommands.ts
+++ src/getCommands.ts
@@ -6,24 +6,30 @@ import { CheckCommand } from "./errorChecker.ts";
 
 import type { Command } from "./types/command.ts";
 
+const commandExtension = /\.ts|\.js$/;
+
 export async function GetCommands() {
 	const commandsDirRaw = process.env.COMMANDS_DIR || "./commands";
 	const commandsDir = isAbsolute(commandsDirRaw) ? commandsDirRaw : join(import.meta.dirname, commandsDirRaw);
 
-	const commandModules = readdirSync(commandsDir).filter(file => file.endsWith(".ts"));
+	const commandModules = readdirSync(commandsDir).filter(file => commandExtension.test(file));
 	const commands = new Collection<string, Command>();
 
 	for (const commandFile of commandModules) {
 		const command = (await import(`file://${join(commandsDir, commandFile)}`))?.command;
 
 		const { isValid, command: validatedCommand } = CheckCommand(command);
-		if (!isValid) {
+		if (isValid) {
+			console.log(`Registered command: ${validatedCommand.data.name}`);
+		} else {
 			console.error(`Invalid command: ${commandFile}`);
 			continue;
 		}
 
 		commands.set(validatedCommand.data.name, validatedCommand);
 	}
+
+	console.log(`Registered ${commands.size} total commands.`);
 
 	return commands;
 }
